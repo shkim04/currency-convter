d0dc96b0300654efc16c6be1def639f282686039
shkim04 <38900501+shkim04@users.noreply.github.com>
2021-12-17 09:29:09 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-SCRIPTNAME="$0"
-ARGS="( $@ )"
 BRANCH="master"
 
 self_update() {
     cd $SCRIPTPATH
     git fetch
 
-    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+    [ -n $(git diff --name-only origin/$BRANCH) ] && {
         echo "Found a new version of me, updating myself..."
         git pull --force
         git checkout $BRANCH
         git pull --force
         echo "Running the new version..."
-        exec "$SCRIPTNAME" "${ARGS[@]}"
 
         # Now exit this old instance
         exit 1
