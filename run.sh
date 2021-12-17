fd4c37f7864222b9223f77d37398aa38934445fb
shkim04 <38900501+shkim04@users.noreply.github.com>
2021-12-17 09:30:13 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTNAME="$0"
 BRANCH="master"
 
 self_update() {
     cd $SCRIPTPATH
     git fetch
 
-    [ -n $(git diff --name-only origin/$BRANCH) ] && {
+    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
         echo "Found a new version of me, updating myself..."
         git pull --force
         git checkout $BRANCH
